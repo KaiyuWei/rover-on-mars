b9ba36bbb1e77ada3e4217d3676eface4a855502
KaiyuWei <weikaiyu2020@outlook.com>
2024-06-04 23:33:33 +0200

    command classes added

---
+++ app/Console/Commands/RobotGo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RobotGo extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'robot:go';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Control the robot movement by instructions';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
